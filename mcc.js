59d2b72420148394377f95caf3369f0913acbfcb
hanhanworld <n000145275@gmail.com>
2024-08-05 23:00:13 +0800

    更新 HTML 及 PDF 文件

--- mcc.js
+++ mcc.js
@@ -1,3 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .highlight {
+            background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+
+<input type="text" id="searchInput" placeholder="Search for equipment...">
+<button onclick="searchTable()">Search</button>
+
+<table id="equipmentTable">
+    <tr>
+        <th>Equipment</th>
+        <th>Details</th>
+    </tr>
+    <tr>
+        <td>Hammer</td>
+        <td>Tool</td>
+    </tr>
+    <tr>
+        <td>Wrench</td>
+        <td>Tool</td>
+    </tr>
+    <tr>
+        <td>Drill</td>
+        <td>Tool</td>
+    </tr>
+    <tr>
+        <td>Saw</td>
+        <td>Tool</td>
+    </tr>
+</table>
+
+<script>
+var currentIndex = 0;
+
 function searchTable() {
     var input, filter, table, tr, td, i, j, txtValue;
     input = document.getElementById("searchInput");
@@ -5,17 +45,39 @@ function searchTable() {
     table = document.getElementById("equipmentTable");
     tr = table.getElementsByTagName("tr");
 
+    // Remove previous highlights
     for (i = 1; i < tr.length; i++) {
         tr[i].classList.remove("highlight");
+    }
+
+    var found = false;
+
+    // Start searching from the current index
+    for (i = currentIndex + 1; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td");
         for (j = 0; j < td.length; j++) {
             if (td[j]) {
                 txtValue = td[j].textContent || td[j].innerText;
                 if (txtValue.toUpperCase().indexOf(filter) > -1) {
                     tr[i].classList.add("highlight");
+                    currentIndex = i;
+                    found = true;
                     break;
                 }
             }
         }
+        if (found) {
+            break;
+        }
+    }
+
+    // If no match found, reset the index and try again
+    if (!found) {
+        currentIndex = 0;
+        searchTable();
     }
 }
+</script>
+
+</body>
+</html>
