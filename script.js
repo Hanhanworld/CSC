ffc5892b6e3b9e7cfa83dc927989acb1169dd8cb
hanhanworld <n000145275@gmail.com>
2024-08-11 23:33:58 +0800

    更新 HTML 及 PDF 文件

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// 监听侧边栏切换按钮的点击事件
 document.querySelector('.sidebar-toggle').addEventListener('click', function() {
     document.querySelector('.sidebar').classList.toggle('show');
+});
+
+// 监听侧边栏内选项的点击事件
+document.querySelectorAll('.sidebar a').forEach(function(link) {
+    link.addEventListener('click', function() {
+        // 在选择完选项后自动关闭侧边栏
+        document.querySelector('.sidebar').classList.remove('show');
+    });
 });
